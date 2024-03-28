e8424ffc6850ad308bd673fc7e94f5c20f0c3d2b
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-03-28 17:48:45 +0530

    first commit

---
+++ libxcode/multiplyLib.swift
@@ -0,0 +1,15 @@
+//
+//  multiplyLib.swift
+//  libxcode
+//
+//  Created by Pooja Babusing on 28/03/24.
+//
+
+import Foundation
+public final class multiplyLib  {
+    public init (){}
+    public static func greet() {
+        print(">>>>>>>>>> Hello from MyFramework!")
+    }
+}
+
