7acd5741fcbd7f428547aa1a526c0cbd9f9e5246
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-04-01 12:46:50 +0530

    fix add swift tool versiob

--- Package.swift
+++ Package.swift
@@ -1,9 +1,5 @@
-//
-//  package.swift
-//  libxcode
-//
-//  Created by Kiruthika Jeyashankar on 01/04/24.
-//
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
